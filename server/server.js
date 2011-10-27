06106e5cf13a1bcde8c573e951a049b605973c50
lauxley <robintissot@free.fr>
2011-10-27 03:16:23 +0200

    ca bouge

--- server/server.js
+++ server/server.js
@@ -2,51 +2,78 @@ var fs = require('fs');
 var path = require('path');
 
 var id = 1;
-var players = {};
+var players = [];
 var sockets = {};
 
 var broadcast = function(type, msg) {
     for(var sId in sockets) {
-	console.log(sId);
-	console.log(sockets[sId]);
 	sockets[sId].emit(type, msg);
     }
 };
 
 var app = require('http').createServer(function (req, res) {
-    fs.readFile(path.normalize(__dirname+'/../client/index.html'),
-    function (err, data) {
-	if (err) {
-	    res.writeHead(500);
-	    return res.end('Error loading index.html');
-	}
+    console.log(req.url);
+    switch (req.url) {
+
+    case '/':
+	fs.readFile(path.normalize(__dirname+'/../client/index.html'),
+	function (err, data) {
+	    if (err) {
+		res.writeHead(500);
+		return res.end('Error loading index.html');
+	    }
+	    res.writeHead(200);
+	    res.end(data);
+	});
+	break;
+    case '/Three.js':
+	fs.readFile(path.normalize(__dirname+'/../client/Three.js'),
+	function (err, data) {
+	    if (err) {
+		res.writeHead(500);
+		return res.end('Error loading Three.js');
+	    }
+	    res.writeHead(200);
+	    res.end(data);
+	});
+	break;
+    default:
 	res.writeHead(200);
-	res.end(data);
-    });
+	res.end('404 MOFO!');
+    }
 }).listen(8000);;
 var io = require('socket.io').listen(app);
 
 io.sockets.on('connection', function (socket) {
     console.log('New client ('+socket.id+')!');
     socket.emit('info', 'welcome !');
-    var player = { 'id':id, 'x':0, 'y':0 };
+    var x = Math.round(Math.random()*3001-1500);
+    var y = Math.round(Math.random()*1561-780);
+    var player = {'id':id, 'x':x, 'y':y };
+    socket.emit('welcome', player);
+    broadcast('new_player', player);
+    socket.player = player;
     sockets[id] = socket;
-    players[socket.id] = player;
+    players.push(player);
+    socket.emit('players', players);
     id++;
     broadcast('info', 'there is now '+Object.keys(players).length+' players in the field!');
-    broadcast('players', players);
 
     socket.on('disconnect', function() {
-	delete sockets[players[this.id].id];
-	delete players[this.id];
+	//broadcast();
+	for(i=0;i<players.length;i++) {
+	    if(players[i] && players[i].id == socket.player.id) {
+		delete sockets[players[i].id];
+		delete players[i];	
+	    }
+	}
     });
 
     socket.on('move', function(data) {
-	player = players[this.id];
-	var p = JSON.parse(data);
-	player.x = p.x;
-	player.y = p.y;
-	broadcast(players);
+	var d = JSON.parse(data);
+	socket.player.x = d.x;
+	socket.player.y = d.y;
+	broadcast('players', players);
     })
 });
 
